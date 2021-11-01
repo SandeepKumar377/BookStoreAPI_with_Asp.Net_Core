8cecab8f940706c3bde3cc563d619f551183945a
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-01 18:47:51 +0530

    Create First getAllbooks list API

--- BookStoreAPI/Repository/BookRepository.cs
+++ BookStoreAPI/Repository/BookRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BookStoreAPI.Data;
+using BookStoreAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,22 @@ namespace BookStoreAPI.Repository
 {
     public class BookRepository : IBookRepository
     {
+        private readonly BookStoreContext _context;
+
+        public BookRepository(BookStoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<BookModel>> GetAllBookAsync()
+        {
+            var records = await _context.Books.Select(x=> new BookModel()
+            { 
+            Id=x.Id,
+            Title=x.Title,
+            Description=x.Description
+            }).ToListAsync();
+
+            return records;
+        }
     }
 }
--- BookStoreAPI/Repository/IBookRepository.cs
+++ BookStoreAPI/Repository/IBookRepository.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using BookStoreAPI.Models;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookStoreAPI.Repository
 {
     public interface IBookRepository
     {
+        Task<List<BookModel>> GetAllBookAsync();
     }
 }

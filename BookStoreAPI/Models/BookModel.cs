0f2a6bb0401a10cf163d9531be1af2b6b9a62615
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-11-07 14:00:09 +0530

    Implement Model validations

--- BookStoreAPI/Models/BookModel.cs
+++ BookStoreAPI/Models/BookModel.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStoreAPI.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter the Title!")]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
